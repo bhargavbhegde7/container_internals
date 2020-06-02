0bf8fbdefc63283950c019fb678eb39aa70b72e8
bhegde <bhargavbhegde7@gmail.com>
2020-06-02 18:22:06 +0530

    making isolate as a clion project

---
+++ Isolate/build.sh
@@ -0,0 +1,1 @@
+/home/bhegde/Downloads/clion-2020.1.1/bin/cmake/linux/bin/cmake --build /home/bhegde/CLionProjects/Isolate/cmake-build-debug --target Isolate -- -j 4
---
+++ Isolate/main.c
@@ -0,0 +1,158 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <sched.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <sys/prctl.h>
+#include <wait.h>
+#include <memory.h>
+
+static void die(const char *fmt, ...)
+{
+    va_list params;
+
+    va_start(params, fmt);
+    vfprintf(stderr, fmt, params);
+    va_end(params);
+    exit(1);
+}
+
+struct params {
+    int fd[2];
+    char **argv;
+};
+
+static void parse_args(int argc, char **argv,
+                       struct params *params)
+{
+#define NEXT_ARG() do { argc--; argv++; } while (0)
+    // Skip binary path
+    NEXT_ARG();
+
+    if (argc < 1) {
+        printf("Nothing to do!\n");
+        exit(0);
+    }
+
+    params->argv = argv;
+#undef NEXT_ARG
+}
+
+#define STACKSIZE (1024*1024)
+static char cmd_stack[STACKSIZE];
+
+void await_setup(int pipe)
+{
+    // We're done once we read something from the pipe.
+    char buf[2];
+    if (read(pipe, buf, 2) != 2)
+        die("Failed to read from pipe: %m\n");
+}
+
+static int cmd_exec(void *arg)
+{
+    // Kill the cmd process if the isolate process dies.
+    if (prctl(PR_SET_PDEATHSIG, SIGKILL))
+        die("cannot PR_SET_PDEATHSIG for child process: %m\n");
+
+    struct params *params = (struct params*) arg;
+    // Wait for 'setup done' signal from the main process.
+    await_setup(params->fd[0]);
+
+    if (setgid(0) == -1)
+        die("Failed to setgid: %m\n");
+    if (setuid(0) == -1)
+        die("Failed to setuid: %m\n");
+
+    char **argv = params->argv;
+    char *cmd = argv[0];
+    printf("===========%s============\n", cmd);
+
+    if (execvp(cmd, argv) == -1)
+        die("Failed to exec %s: %m\n", cmd);
+
+    die("¯\\_(ツ)_/¯");
+    return 1;
+}
+
+static void write_file(char path[100], char line[100])
+{
+    FILE *f = fopen(path, "w");
+
+    if (f == NULL) {
+        die("Failed to open file %s: %m\n", path);
+    }
+
+    if (fwrite(line, 1, strlen(line), f) < 0) {
+        die("Failed to write to file %s:\n", path);
+    }
+
+    if (fclose(f) != 0) {
+        die("Failed to close file %s: %m\n", path);
+    }
+}
+
+
+static void prepare_userns(int pid)
+{
+    char path[100];
+    char line[100];
+
+    int uid = 1000;
+
+    sprintf(path, "/proc/%d/uid_map", pid);
+    sprintf(line, "0 %d 1\n", uid);
+    write_file(path, line);
+
+    sprintf(path, "/proc/%d/setgroups", pid);
+    sprintf(line, "deny");
+    write_file(path, line);
+
+    sprintf(path, "/proc/%d/gid_map", pid);
+    sprintf(line, "0 %d 1\n", uid);
+    write_file(path, line);
+}
+
+int main(int argc, char **argv)
+{
+
+    struct params params;
+    memset(&params, 0, sizeof(struct params));
+
+    parse_args(argc, argv, &params);
+
+    // Create pipe to communicate between main and command process.
+    if (pipe(params.fd) < 0)
+        die("Failed to create pipe: %m");
+
+    // Clone command process.
+    int clone_flags =
+            // if the command process exits, it leaves an exit status
+            // so that we can reap it.
+            SIGCHLD |
+            CLONE_NEWUTS |
+            CLONE_NEWUSER;
+    int cmd_pid = clone(
+            cmd_exec, cmd_stack + STACKSIZE, clone_flags, &params);
+
+    if (cmd_pid < 0)
+        die("Failed to clone: %m\n");
+
+    // Get the writable end of the pipe.
+    int pipe = params.fd[1];
+
+    // Some namespace setup will take place here ...
+    prepare_userns(cmd_pid);
+
+    // Signal to the command process we're done with setup.
+    if (write(pipe, "OK", 2) != 2)
+        die("Failed to write to pipe: %m");
+    if (close(pipe))
+        die("Failed to close pipe: %m");
+
+    if (waitpid(cmd_pid, NULL, 0) == -1)
+        die("Failed to wait pid %d: %m\n", cmd_pid);
+
+    return 0;
+}
